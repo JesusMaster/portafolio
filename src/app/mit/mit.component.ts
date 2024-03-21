ac0a9e1a48d365251b02bd3d32a8926633ef4837
Jesus Leiva <saxofonista7@gmail.com>
2024-03-20 21:24:00 -0300

    change: add other sections

---
+++ src/app/mit/mit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mit',
+  standalone: true,
+  imports: [],
+  templateUrl: './mit.component.html',
+  styleUrl: './mit.component.scss'
+})
+export class MitComponent {
+
+}
